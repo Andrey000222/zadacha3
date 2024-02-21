52fb8a356dc97eeab2b665f563bb40ad67c0823f
Andrey000222 <113039378+Andrey000222@users.noreply.github.com>
2024-02-21 12:20:44 +0300

    Add files via upload

--- kod.cpp
+++ kod.cpp
@@ -1,51 +1,32 @@
 #include"kod.h"
 IntegerArray::IntegerArray(int size_) :size_array(size_) {
-	try
+	if (size_ <= 0)
 	{
-		if (size_ < 0)
-		{
-			throw "the size of the array must be greater than 0";
-		}
-		array = new int(size_);
-		cout << "the array has been created" << endl;
+		throw Error_1();
 	}
-	catch (const char error_text)
-	{
-		cout << error_text << endl;
-	}
+	array = new int(size_);
+	cout << "the array has been created" << endl;
 }
-void IntegerArray::new_array_size(int size_)  { 
-	try
-	{
-		if (0 >= size_) {
-			throw "the array must be greater than 0" ;
-		}
-		int* array_;
-		array_ = new int(size_);
-		std::copy_n(array, size_, array_);
-		delete[] array;
-		size_array = size_;
-		array = array_;
-		cout << "new array size" << endl;
+void IntegerArray::new_array_size(int size_) {
+	if (size_ <= 0) {
+		throw Error_1();
 	}
-	catch (const char error_text)
-	{
-		cout << error_text << endl;
-	}
-};
+	int* array_;
+	array_ = new int(size_);
+	std::copy_n(array, size_, array_);
+	delete[] array;
+	size_array = size_;
+	array = array_;
+	cout << "new array size" << endl;
+}
 void IntegerArray::chang_num_array(int nam) {
-	try {
-		if ((nam <= 0) && (nam < size_array)) {
-			throw"the value must be within the array";
-		}
-		int nam_1;
-		cin >> nam_1;
-		cout << endl;
-		array[(nam-1)] = nam_1;
+	if ((nam <= 0) || (nam > size_array)) {
+		throw Error_2();
 	}
-	catch (const char error_text) {
-		cout << error_text << endl;
-	};
+	int nam_1;
+	cin >> nam_1;
+	cout << endl;
+	array[(nam - 1)] = nam_1;
 }
 void IntegerArray::enum_array() {
 	for (size_t i = 0; i < size_array; i++)
@@ -63,30 +44,21 @@ IntegerArray::IntegerArray(IntegerArray a,int size_) {
 		cout << "a new array has been created and an array has been copied into it" << endl;
 };
 void IntegerArray::remov_elem_array(int nam) {
-	try
+
+	if ((nam > size_array)||(nam <= 0)) {
+		throw Error_2();
+	};
+	int* array_ = new int[size_array - 1];
+	for (size_t i = 0; i < (nam - 1); i++)
 	{
-		if (nam > size_array) {
-			throw "the number is larger than the size of the array";
-		}
-		if (nam == 0) {
-			throw"nam = 0";
-		};
-		int* array_ = new int[size_array-1];
-		for (size_t i = 0; i < (nam - 1); i++)
-		{
-			array_[i] = array[i];
-		};
-		for (size_t i = nam; i < size_array; i++)
-		{
-			array_[i - 1] = array[i];
-		};
-		size_array -= 1;
-		array = array_;
-	}
-	catch (const char error_text)
+		array_[i] = array[i];
+	};
+	for (size_t i = nam; i < size_array; i++)
 	{
-		cout << error_text << endl;
-	}
+		array_[i - 1] = array[i];
+	};
+	size_array -= 1;
+	array = array_;
 
 }
 void IntegerArray::search_nam(int nam) {
--- kod.h
+++ kod.h
@@ -16,3 +16,15 @@ protected:
 	int size_array{};
 	int* array{};
 };
+class Error_1 {
+public:
+	const char* what() {
+		return "the array must be greater than 0";
+	}
+};
+class Error_2 {
+public:
+	const char* what() {
+		return "the value must be within the array" ;
+	}
+};
